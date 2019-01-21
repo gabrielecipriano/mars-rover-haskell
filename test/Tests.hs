93fb4c96e2c58217997f0467e9ec5f78cfd1450a
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-01-21 19:39:06 +0000

    defined direction

--- test/Tests.hs
+++ test/Tests.hs
@@ -7,9 +7,12 @@ main = hspecWith defaultConfig {configFastFail = True} specs
 newtype Position = Position (Int, Int)
     deriving (Show, Eq)
 
-moveRover :: (Position, Char) -> String -> (Position, Char)
+data Direction = N | E | S | W
+    deriving (Show, Eq)
+
+moveRover :: (Position, Direction) -> String -> (Position, Direction)
 moveRover rover _ = rover
 
 specs :: Spec
 specs = it "should leave a rover where it is with no commands" $
-          moveRover (Position (0, 0), 'N') "" `shouldBe` (Position (0, 0), 'N')
+        moveRover (Position (0, 0), N) "" `shouldBe` (Position (0, 0), N)
