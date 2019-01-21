2334788a645f63f937b154d36cbb309e234167b6
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-01-21 19:22:06 +0000

    rover never moves

--- test/Tests.hs
+++ test/Tests.hs
@@ -4,9 +4,9 @@ import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
-hello :: String
-hello = "Hello, World!"
+moveRover :: ((Int, Int), Char) -> String -> ((Int, Int), Char)
+moveRover rover _ = rover
 
 specs :: Spec
-specs = it "hello" $
-          hello `shouldBe` "Hello, World!"
+specs = it "should leave a rover where it is with no commands" $
+          moveRover ((0, 0), 'N') "" `shouldBe` ((0, 0), 'N')
