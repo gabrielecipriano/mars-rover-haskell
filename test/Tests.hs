9b037c21c357c06103a5d03b3108913717afbe89
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-01-21 19:27:08 +0000

    extracted position type

--- test/Tests.hs
+++ test/Tests.hs
@@ -4,9 +4,12 @@ import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
-moveRover :: ((Int, Int), Char) -> String -> ((Int, Int), Char)
+newtype Position = Position (Int, Int)
+    deriving (Show, Eq)
+
+moveRover :: (Position, Char) -> String -> (Position, Char)
 moveRover rover _ = rover
 
 specs :: Spec
 specs = it "should leave a rover where it is with no commands" $
-          moveRover ((0, 0), 'N') "" `shouldBe` ((0, 0), 'N')
+          moveRover (Position (0, 0), 'N') "" `shouldBe` (Position (0, 0), 'N')
